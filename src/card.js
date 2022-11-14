c89d6819428cdd164f9780b5617801020c9c2c80
DhivyaSankameswaran <68198970+DhivyaSankameswaran@users.noreply.github.com>
2022-11-14 09:33:05 +0530

    Add files via upload

---
+++ src/card.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css'
+
+function App() {
+  return (
+    <>
+    <div id="container">
+    <h2>BELGIAN WAFFLE</h2>
+<div class="card">
+  <img src="https://media-cdn.tripadvisor.com/media/photo-p/0c/9f/8f/2d/photo0jpg.jpg" alt="Lago di Braies"></img>
+
+  
+  <div class="card__details">
+
+    
+    <span class="tag">Crispiness</span>
+
+    <span class="tag">On-the-go Treat</span>
+
+    
+    <div class="name">BELGIAN WAFFLE</div>
+
+    <p>Our waffles are freshly baked, hot and crispy and come in a variety of batters and fillings!
+       A burning passion for quality, consistency, novelty and adding value in the QSR space is the reason that our food franchise, The Belgian Waffle Co., came to being.The concept of freshly baked waffles being offered by an on-the-go waffle restaurant in India particularly attracted him.
+
+</p>
+
+    <button>Read more</button>
+  </div>
+
+
+</div>
+</div>
+
+    </>
+  );
+}
+
+export default App;
